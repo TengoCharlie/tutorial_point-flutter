2fd61192b0d1e3708629f1ea10b73e08133332b4
Harsh Tenguriya <thegreatharsh1@gmail.com>
2022-10-19 03:06:56 +0530

    Firebase cloud databse working

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,27 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:tutorial_point_learn/src/database/database.dart';
+import 'package:tutorial_point_learn/firebase_options.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:tutorial_point_learn/src/service/Product.dart';
 import 'package:tutorial_point_learn/src/widget/product_box_list.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp(
-      products:
-          SQLiteDbProvider.db.getAll("Product", columns: Product.columns)));
+    products: fetchProducts(),
+  ));
+}
+
+Stream<QuerySnapshot> fetchProducts() {
+  return FirebaseFirestore.instance.collection('Product').snapshots();
 }
 
 class MyApp extends StatelessWidget {
-  final Future<List<Product>>? products;
+  final Stream<QuerySnapshot>? products;
   const MyApp({Key? key, this.products}) : super(key: key);
 
   @override
@@ -30,7 +38,7 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  final Future<List<Product>>? products;
+  final Stream<QuerySnapshot>? products;
   const MyHomePage({super.key, this.products, required this.title});
 
   @override
@@ -40,15 +48,31 @@ class MyHomePage extends StatelessWidget {
           title: const Text("My App rest"),
         ),
         body: Center(
-          child: FutureBuilder<List<Product>>(
-            future: products,
+          child: StreamBuilder<QuerySnapshot>(
+            stream: products,
             builder: (context, AsyncSnapshot snapshot) {
               if (snapshot.hasError) {
                 print(snapshot.error);
               }
-              return snapshot.hasData
-                  ? ProductBoxList(items: snapshot.data)
-                  : const Center(child: CircularProgressIndicator());
+              if (snapshot.hasData) {
+               if(snapshot.data.docs.length>0) {
+                  List<DocumentSnapshot> documents = snapshot.data.docs;
+                  List<Product> items = <Product>[];
+                  for (var i = 0; i < documents.length; i++) {
+                    DocumentSnapshot document = documents[i];
+                    print(document.data());
+                    items.add(Product.fromJson(
+                        document.data() as Map<String, dynamic>));
+                  }
+                  return ProductBoxList(items: items);
+                }
+               else {
+                 return Center(child: CircularProgressIndicator());
+               }
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+              // const Center(child: CircularProgressIndicator());
             },
           ),
         ));
--- lib/src/service/Product.dart
+++ lib/src/service/Product.dart
@@ -1,15 +1,13 @@
 class Product {
-  final String id;
   final String name;
   final String description;
   final int price;
   final String image;
-  static final columns = ["id", "name", "description", "price", "image"];
-  Product(this.id, this.name, this.description, this.price, this.image);
+  static final columns = [ "name", "description", "price", "image"];
+  Product( this.name, this.description, this.price, this.image);
 
   factory Product.fromJson(Map<String, dynamic> json) {
     return Product(
-      json['id'],
       json['name'],
       json['description'],
       json['price'],
@@ -17,7 +15,7 @@ class Product {
     );
   }
   Map<String, dynamic> toJson() => {
-        "id": id,
+
         "name": name,
         "description": description,
         "price": price,
